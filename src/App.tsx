06c55815f76b9c9232dd8aba1426a7e5e5eb32a0
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2024-11-11 16:31:02 +0900

    works jsonfile作成

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,27 @@ import './assets/styles/style.css';
 import Particles from './components/Particles';
 
 const App: React.FC = () => {
-//   const worksData = {
-//     worksWebsite: [
-//       { id: '1', title: 'Website Project A', image_path: 'website_a' },
-//       { id: '2', title: 'Website Project B', image_path: 'website_b' },
-//     ],
-//     worksApp: [
-//       { id: '3', title: 'App Project A', image_path: 'app_a' },
-//       { id: '4', title: 'App Project B', image_path: 'app_b' },
-//     ],
-//     worksOther: [
-//       { id: '5', title: 'Other Project A', image_path: 'other_a' },
-//     ],
-//   };
+  const worksData = {
+    worksWebsite: [
+      { id: '1', title: 'Website Project A', image_path: 'website_a' },
+      { id: '2', title: 'Website Project B', image_path: 'website_b' },
+    ],
+    worksApp: [
+      { id: '3', title: 'App Project A', image_path: 'app_a' },
+      { id: '4', title: 'App Project B', image_path: 'app_b' },
+    ],
+    worksOther: [
+      { id: '5', title: 'Other Project A', image_path: 'other_a' },
+    ],
+  };
 
   return (
     <Router>
         <Particles />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/works" element={<Works />} />
-          {/* <Route path="/works" element={<Works {...worksData} />} /> */}
+          {/* <Route path="/works" element={<Works />} /> */}
+          <Route path="/works" element={<Works {...worksData} />} />
           <Route path="/service" element={<Service />} />
           <Route path="/blog" element={<Blog />} />
           {/* <Route path="/blog" element={<Blog {...blogData} />} /> */}
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,112 +1,119 @@
 import Layout from '../components/Layout';
+import worksData from '../data/worksData.json';
 
-// interface Work {
-// 	id: string;
-// 	title: string;
-// 	image_path: string;
-// }
+interface Work {
+	id: string;
+	title: string;
+	image_path: string;
+}
 
-// interface WorksProps {
-// 	worksWebsite: Work[];
-// 	worksApp: Work[];
-// 	worksOther: Work[];
-// }
+interface WorksProps {
+	worksWebsite: Work[];
+	worksApp: Work[];
+	worksOther: Work[];
+}
 
-export default function Works() {
-
+export default function Works({
+	worksWebsite = worksData.worksWebsite,
+	worksApp = worksData.worksApp,
+	worksOther = worksData.worksOther,
+}: WorksProps) {
 	return (
 		<Layout title="works">
 			<p>wa-kusu</p>
-			{/* <div id="works-slide" className="tab"> */}
+			<div id="works-slide" className="tab">
 				{/* Website Tab */}
-				{/* <label>
+				<label>
 					<input type="radio" name="works" defaultChecked />
 					Web site
 				</label>
 				<div className="swiper works-wraper">
 					<ul className="swiper-wrapper">
-						<li key="1" className="swiper-slide">
-							<a href="/works/1" className="slide-item">
-								<picture>
-								*/}
-									{/* <source
-										srcSet="../assets/images/works-cliantA.webp"
-										type="image/webp"
-									/>
-									<img
-										src="../assets/images/works-cliantA.png"
-										alt="クライアントA様のサイトイメージ"
-										height="300"
-										loading="lazy"
-									/>
-								</picture>
-							</a>
-						</li>
+						{worksWebsite.map((work) => (
+							<li key={work.id} className="swiper-slide">
+								<a href={`/works/${work.id}`} className="slide-item">
+									<picture>
+										<source
+											srcSet={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.webp`}
+											type="image/webp"
+										/>
+										<img
+											src={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.png`}
+											alt={`${work.title}のサイトイメージ`}
+											height="300"
+											loading="lazy"
+										/>
+									</picture>
+								</a>
+							</li>
+						))}
 					</ul>
 					<div className="swiper-button-prev"></div>
 					<div className="swiper-button-next"></div>
-				</div> */}
+				</div>
 
 				{/* App Tab */}
-				{/* <label>
+				<label>
 					<input type="radio" name="works" />
 					App
 				</label>
 				<div className="swiper works-wraper">
 					<ul className="swiper-wrapper">
-						<li key="3" className="swiper-slide">
-							<a href="/works/3" className="slide-item">
-								<picture>
-									<source
-										srcSet=""
-										type="image/webp"
-									/>
-									<img
-										src=""
-										alt="家族マネー会議のサイトイメージ"
-										width="700"
-										height="300"
-										loading="lazy"
-									/>
-								</picture>
-							</a>
-						</li>
+						{worksApp.map((work) => (
+							<li key={work.id} className="swiper-slide">
+								<a href={`/works/${work.id}`} className="slide-item">
+									<picture>
+										<source
+											srcSet={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.webp`}
+											type="image/webp"
+										/>
+										<img
+											src={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.png`}
+											alt={`${work.title}のアプリイメージ`}
+											width="700"
+											height="300"
+											loading="lazy"
+										/>
+									</picture>
+								</a>
+							</li>
+						))}
 					</ul>
 					<div className="swiper-button-prev"></div>
 					<div className="swiper-button-next"></div>
-				</div> */}
+				</div>
 
 				{/* Other Tab */}
-				{/* <label>
+				<label>
 					<input type="radio" name="works" />
 					Other
 				</label>
 				<div className="swiper works-wraper">
-							<ul className="swiper-wrapper">
-									<li key="" className="swiper-slide">
-										<a href="#" className="slide-item">
-											<picture>
-												<source
-													srcSet={`${publicUrl}/assets/images/${work.image_path}.webp`}
-													type="image/webp"
-												/>
-												<img
-													src={`${publicUrl}/assets/images/${work.image_path}.png`}
-													alt={`${work.title}のサイトイメージ`}
-													width="700"
-													height="300"
-													loading="lazy"
-												/>
-											</picture>
-										</a>
-									</li>
-							</ul>
-							<div className="swiper-button-prev"></div>
-							<div className="swiper-button-next"></div>
-				</div> */}
-			{/* </div>
-		</section> */}
+					<ul className="swiper-wrapper">
+						{worksOther.map((work) => (
+							<li key={work.id} className="swiper-slide">
+								<a href={`/works/${work.id}`} className="slide-item">
+									<picture>
+										<source
+											srcSet={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.webp`}
+											type="image/webp"
+										/>
+										<img
+											src={`${process.env.PUBLIC_URL}/assets/images/${work.image_path}.png`}
+											alt={`${work.title}のその他イメージ`}
+											width="700"
+											height="300"
+											loading="lazy"
+										/>
+									</picture>
+								</a>
+							</li>
+						))}
+					</ul>
+					<div className="swiper-button-prev"></div>
+					<div className="swiper-button-next"></div>
+				</div>
+			</div>
 		</Layout>
-
 	);
 }
